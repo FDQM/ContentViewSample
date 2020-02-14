1c56a7fe2d05ed547993a2ca2987f06b70931bd0
FDQM98 <franklindavid.quezadamartinez@gmail.com>
2020-02-13 21:32:26 -0400

    no message

---
+++ AuthenticationSample/AuthenticationSample/LoginPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AuthenticationSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void ButtonGoSignUp(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(EntryEmail.Text) || String.IsNullOrEmpty(PasswordEntry.Text))
+            {
+                await DisplayAlert("Alert", "Password or Email Empty", "OK");
+            }
+            else
+            {
+                await Navigation.PushAsync(new SignUpPage(EntryEmail.Text));
+            }
+        }
+    }
+}
---
+++ AuthenticationSample/AuthenticationSample/SignUpPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AuthenticationSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUpPage : ContentPage
+    {
+        public SignUpPage(String Email)
+        {
+            InitializeComponent();
+            NewEmail.Text = Email;
+        }
+
+        async void GoToMenu(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(NewEmail.Text) || String.IsNullOrEmpty(NewPassword.Text) || String.IsNullOrEmpty(RepeatNewPassword.Text))
+            {
+                await DisplayAlert("Alert", "Password or Email Empty", "OK");
+            }
+            else
+            {
+                await Navigation.PushAsync(new MenuPage());
+            }
+        }
+    }
+}
